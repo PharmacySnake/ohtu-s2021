69fde78a62f0400276ea0d614810cfc9514d8263
Andreas Katsaras <andreas.katsaras@gmail.com>
2021-11-05 04:50:53 +0200

    viikkotehtavat 14-16

---
+++ viikko1/tehtavat14-16/NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -0,0 +1,29 @@
+package ohtuesimerkki;
+
+import org.junit.Before;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StatisticsTest {
+    Reader readerStub = new Reader() {
+        public List<Player> getPlayers() {
+            ArrayList<Player> players = new ArrayList<>();
+
+            players.add(new Player("Semenko", "EDM",4,12));
+            players.add(new Player("Lemieux", "PIT",45,54));
+            players.add(new Player("Kurri", "EDM",37,53));
+            players.add(new Player("Yzerman", "DET",42,56));
+            players.add(new Player("Gretzky", "EDM",35,89));
+
+            return players;
+        }
+    };
+
+    Statistics stats;
+
+    @Before
+    public void setUp() {
+        stats = new Statistics(readerStub);
+    }
+}
